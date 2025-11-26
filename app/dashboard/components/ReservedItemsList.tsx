902ae823f87b979e9aa5ece6fd0dfea0b3d7d1d4
aivaraslas <a.lasovas@gmail.com>
2025-11-26 23:15:41 +0200

    Minor update, to test how it works: made empty state message alignment consistent between the Reserved Items List and the Follow List
    ALSO: Some changes were made to pnpm-workspace, but I'm not sure what's that :D

--- app/dashboard/components/ReservedItemsList.tsx
+++ app/dashboard/components/ReservedItemsList.tsx
@@ -9,7 +9,7 @@ export function ReservedItemsList({ items }: { items: Item[] }) {
   return (
     <div>
       {items.length === 0 ? (
-        <div className="flex flex-col justify-center items-center">
+        <div className="flex flex-col justify-center items-start mt-4">
           <p className="text-center">{t("noItems")}</p>
         </div>
       ) : null}
